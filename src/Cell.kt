edcf23eb750fbc11b52b200f98f5e447eb633e86
Ahmed AbdulKarim <ahmedtechno93@gmail.com>
2021-04-09 20:15:06 +0300

    convert java to kotlin by use IntelliJ IDEA configuration

--- src/Cell.kt
+++ src/Cell.kt
@@ -1,29 +1,17 @@
-public class Cell {
-
-  //This is one cell of the game grid
-  
-  public String contents;
-  public boolean empty;
-  
-  public Cell() {
-  
-    this.contents = " ";
-    this.empty = true;
-  }
-  
-  public String output() {
-  
-    return this.contents;
-  }
-  
-  public void placeMark() {
-  
-    if(TicTacToe.count%2==0){
-      this.contents = "X";
-    } else {
-      this.contents = "O";
+class Cell {
+    //This is one cell of the game grid
+    var contents = " "
+    var empty = true
+    fun output(): String {
+        return contents
     }
-    
-    this.empty = false;
-  }
+
+    fun placeMark() {
+        if (TicTacToe.count % 2 === 0) {
+            contents = "X"
+        } else {
+            contents = "O"
+        }
+        empty = false
+    }
 }
--- src/TicTacToe.kt
+++ src/TicTacToe.kt
@@ -1,167 +1,121 @@
-/*
- * Write a tic-tac-toe program where a human user
- * can play against  an AI bot, and where two  AI 
- * bots can play against each other.  Write input
- * and output code so that it's intuitive for the 
- * user.
- */
+import java.io.*
+import java.util.ArrayList
+import java.util.Random
+import java.util.regex.*
 
-import java.io.*;
-import java.util.ArrayList;
-import java.util.Random;
-import java.util.regex.*;
+object TicTacToe {
+    var game: Game? = null
+    var count = 0
+    var user_input: String? = null
+    private var gameMode = 0
+    private var valid_input = false
+    fun main(args: Array<String?>?) {
+        val gameSize: Int
+        val minimumGameSize = 1
+        val maximumGameSize = 26
 
-public class TicTacToe {
+        //When program starts, user is met with a welcome message
+        System.out.println("\n\tمرحبا بكم في لعبة اكس او\"")
+        System.out.println("\n\tPlease select your Game mode.")
+        System.out.println("\n\t    (1) Human vs. Computer")
+        System.out.println("\n\t    (2) Computer vs. Computer")
+        user_input = getInput("\n\tWhich mode would you like to play? (1/2): ")
 
-  public static Game game;
-  public static int count = 0;
-  public static String user_input;
-  
-  private static int gameMode;
-  private static boolean valid_input;
-  
-  public static void main(String[] args){
-  
-    int gameSize;
-    int minimumGameSize = 1;
-    int maximumGameSize = 26;
-  
-    //When program starts, user is met with a welcome message
-    System.out.println("\n\tمرحبا بكم في لعبة اكس او\"");
-    System.out.println("\n\tPlease select your Game mode.");
-    System.out.println("\n\t    (1) Human vs. Computer");
-    System.out.println("\n\t    (2) Computer vs. Computer");
-    user_input = getInput("\n\tWhich mode would you like to play? (1/2): ");
-    
-    //Keep asking for an answer from the user until we get a 1 or a 2
-    gameMode(user_input); //gameMode() is defines below
-    
-    System.out.println("\n\tHow large of a grid would you like to use? ");
-    user_input = getInput("\n\tPlease enter an integer between " + minimumGameSize + " and " + maximumGameSize + ": ");
-    
-    //validate user unput for game size
-    valid_input = false;
-    while(!valid_input){
-    
-      if( user_input.length() > 0 && (user_input.substring(0, 1).matches("[1-9]")) && (minimumGameSize <= Integer.parseInt(user_input)) && (Integer.parseInt(user_input) <= maximumGameSize) ){
-      
-        valid_input = true;
-        
-      } else {
-      
-        user_input = getInput("\n\tYou must enter a number between " + minimumGameSize + " and " + maximumGameSize + ": ");
-        
-      }
-    }
-    
-    //issue warning for game sizes larger than 15
-    if(Integer.parseInt(user_input) > 15){
-    
-      System.out.println("\n\t!!WARNING!!\n\t!!WARNING!!  Games large than 15 will not display correctly if console width is restricted to 80 col (neither will this message)\n\t!!WARNING!!");
-      getInput("");
-      
-    }
-    
-    gameSize = Integer.parseInt(user_input);
-    
-    //Create a new Game instance
-    game = new Game(gameSize);
-    
-    //create an array of two players
-    Player[] players = new Player[2];
-    
-    //set players to AI or Human depending on game mode
-    if(gameMode==1){
-    
-      players[0] = new Player("Human");
-      players[1] = new Player("AI");
-      
-    } else {
-    
-      players[0] = new Player("AI");
-      players[1] = new Player("AI");
-      
-    }
-    
-    //Draw the blank board initially to show user which columns and rows to choose from
-    System.out.println(game.output());
-    
-    //until the game is over, go back and forth between players in players array
-    //output the game map to the screen after each move
-    while (!game.finished) {
-    
-      for(Player player : players){
-      
-        player.go();
-        System.out.println("\n" + game.output());
-        count += 1;
-        
-        if(game.finished){
-          break;
+        //Keep asking for an answer from the user until we get a 1 or a 2
+        gameMode(user_input) //gameMode() is defines below
+        System.out.println("\n\tHow large of a grid would you like to use? ")
+        user_input = getInput("\n\tPlease enter an integer between $minimumGameSize and $maximumGameSize: ")
+
+        //validate user unput for game size
+        valid_input = false
+        while (!valid_input) {
+            if (user_input!!.length() > 0 && user_input.substring(0, 1).matches("[1-9]") && minimumGameSize <= Integer.parseInt(user_input) && Integer.parseInt(user_input) <= maximumGameSize) {
+                valid_input = true
+            } else {
+                user_input = getInput("\n\tYou must enter a number between $minimumGameSize and $maximumGameSize: ")
+            }
         }
-        
-      }
+
+        //issue warning for game sizes larger than 15
+        if (Integer.parseInt(user_input) > 15) {
+            System.out.println("\n\t!!WARNING!!\n\t!!WARNING!!  Games large than 15 will not display correctly if console width is restricted to 80 col (neither will this message)\n\t!!WARNING!!")
+            getInput("")
+        }
+        gameSize = Integer.parseInt(user_input)
+
+        //Create a new Game instance
+        game = Game(gameSize)
+
+        //create an array of two players
+        val players: Array<Player?> = arrayOfNulls<Player>(2)
+
+        //set players to AI or Human depending on game mode
+        if (gameMode == 1) {
+            players[0] = Player("Human")
+            players[1] = Player("AI")
+        } else {
+            players[0] = Player("AI")
+            players[1] = Player("AI")
+        }
+
+        //Draw the blank board initially to show user which columns and rows to choose from
+        System.out.println(game.output())
+
+        //until the game is over, go back and forth between players in players array
+        //output the game map to the screen after each move
+        while (!game.finished) {
+            for (player in players) {
+                player!!.go()
+                System.out.println("""
+    
+    ${game.output()}
+    """.trimIndent())
+                count += 1
+                if (game.finished) {
+                    break
+                }
+            }
+        }
+
+        //output an ending message to the game
+        if (game.draw) {
+            System.out.println("\n\tCat's game!")
+        } else {
+
+            //count variable from earlier is used to decide who went last and therefore won.
+            if (count % 2 == 1) {
+                System.out.println("\n\tX's win!")
+            } else {
+                System.out.println("\n\tO's win!")
+            }
+        }
     }
-    
-    //output an ending message to the game
-    if (game.draw) {
-    
-      System.out.println("\n\tCat's game!");
-      
-    } else {
-    
-      //count variable from earlier is used to decide who went last and therefore won.
-      if(count%2==1){
-      
-        System.out.println("\n\tX's win!");
-        
-      } else {
-      
-        System.out.println("\n\tO's win!");
-        
-      }
+
+    //encapsulated code for input stream buffer
+    fun getInput(prompt: String?): String {
+        val stdin = BufferedReader(InputStreamReader(System.`in`))
+        System.out.print(prompt)
+        System.out.flush()
+        return try {
+            stdin.readLine()
+        } catch (e: Exception) {
+            "Error: " + e.getMessage()
+        }
     }
-  }
-  
-  //encapsulated code for input stream buffer
-  public static String getInput(String prompt) {
-  
-    BufferedReader stdin = new BufferedReader( new InputStreamReader(System.in));
-    
-    System.out.print(prompt);
-    System.out.flush();
-    
-    try {
-    
-      return stdin.readLine();
-      
-    } catch (Exception e) {
-    
-      return "Error: " + e.getMessage();
-      
+
+    //validates user input and sets the game mode
+    private fun gameMode(user_input: String?) {
+        var user_input = user_input
+        valid_input = false
+        while (!valid_input) {
+            if (user_input!!.length() === 1 && user_input.substring(0, 1).matches("[1-2]")) {
+                valid_input = true
+            } else {
+                user_input = getInput("\n\tYou must enter '1' or '2' for the game mode: ")
+            }
+        }
+
+        //Set user input to gameMode for use later
+        gameMode = Integer.parseInt(user_input)
     }
-  }
-  
-  //validates user input and sets the game mode
-  private static void gameMode(String user_input) {
-    
-    valid_input = false;
-    
-    while(!valid_input){
-    
-      if((user_input.length() == 1) && (user_input.substring(0,1).matches("[1-2]"))){
-      
-        valid_input = true;
-        
-      } else {
-      
-        user_input = getInput("\n\tYou must enter '1' or '2' for the game mode: ");
-        
-      }
-    }
-    
-    //Set user input to gameMode for use later
-    gameMode = Integer.parseInt(user_input);
-    
-  }
 }
